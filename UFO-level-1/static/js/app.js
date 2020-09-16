aaa46d9769d70c0c637ef518596772e5fbb2a4f7
preeti0103 <64617207+preeti0103@users.noreply.github.com>
2020-09-15 21:40:38 -0400

    code for UFO1

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,56 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Get a reference to the table body
+var tbody = d3.select("tbody");
+
+// Create a function to display the table of data
+function tableBuild(ufoSightData){
+    // Remove any data from the table body
+    tbody.html("");
+    //Loop Through `data` and console.log each UFO report object
+    ufoSightData.forEach(function(ufoData){  
+        //Use d3 to append one table row `tr` for each UFO report object
+        var row = tbody.append("tr");
+        //Use `Object.entries` to console.log each UFO report value
+        Object.entries(ufoData).forEach(function([key, value]){  
+            //Use d3 to append 1 cell per UFO report value (datetime, city, state, country, shape, durationMinutes, comments)
+            var cell = row.append("td");
+            //Use d3 to update each cell's text with UFO report values (datetime, city, state, country, shape, durationMinutes, comments)
+            cell.text(value);
+        })
+    })
+};
+
+
+// Complete the event handler function for the form
+function runEnter() {
+    // Prevent the page from refreshing
+    d3.event.preventDefault();
+    // Select the input element, get the raw HTML node and get the value property of the input element
+    var dates = d3.select("#datetime").property("value");
+    // Create a copy of the data
+    var filteredData = tableData;
+    // If date provided is True, then..
+    if (dates){
+        // Filter the data to match the date provided by the user
+        filteredData = filteredData.filter(sightDate => sightDate.datetime === dates);
+        console.log(filteredData);
+    }
+    // Call the tableBuild function and pass the filtered data with matching date provided by the user
+    tableBuild(filteredData);
+    // log the filtered data
+    console.log(filteredData);
+};
+
+// Select the button
+var filterButton = d3.selectAll("#filter-btn");
+// Select the form
+var form = d3.selectAll("#form");
+
+// Create event handlers 
+filterButton.on("click", runEnter);
+form.on("submit", runEnter);
+
+// Call the function 'tableBuild' to get the original data
+tableBuild(tableData);
--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,62 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+var tbody = d3.select("tbody");
+
+function tableBuild(ufoSightData){
+
+    tbody.html("");
+
+    // Step 1: Loop Through `data` and console.log each UFO report object
+    ufoSightData.forEach(function(ufoData){
+        
+        // Step 2:  Use d3 to append one table row `tr` for each UFO report object
+        var row = tbody.append("tr");
+    
+        // Step 3:  Use `Object.entries` to console.log each UFO report value
+        Object.entries(ufoData).forEach(function([key, value]){   
+
+            // Step 4: Use d3 to append 1 cell per UFO report value (datetime, city, state, country, shape, durationMinutes, comments)
+            var cell = row.append("td");
+
+            // Step 5: Use d3 to update each cell's text with UFO report values (datetime, city, state, country, shape, durationMinutes, comments)
+            cell.text(value);
+        })
+
+    })
+
+};
+
+
+var filterButton = d3.selectAll("#filter-btn");
+var form = d3.selectAll("#form");
+
+
+function runEnter() {
+
+    d3.event.preventDefault();
+
+    var dates = d3.select("#datetime").property("value");
+    var cities = d3.select("#ufo-city").property("value");
+    // var state = d3.select("#ufo-state").property("value");
+    // var country = d3.select("#ufo-country").property("value");
+    // var shape = d3.select("#ufo-shape").property("value");
+
+    var filteredData = tableData;
+
+    if (dates){
+
+        filteredData = filteredData.filter((sightDate => sightDate.datetime === dates));
+        console.log(filteredData);
+    }
+
+    tableBuild(filteredData);
+    console.log(filteredData);
+
+};
+
+filterButton.on("click", runEnter);
+form.on("submit", runEnter);
+
+tableBuild(tableData);
+
